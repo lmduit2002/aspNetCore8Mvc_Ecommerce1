a5045e42c40bd83ae4aa37793d89ce9557544192
lmduit2002 <leminhdai.uit.2002@gmail.com>
2024-11-06 22:26:20 +0700

    Add db context

---
+++ aspNetCore8Mvc_Ecommerce1/Data/TrangWeb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace aspNetCore8Mvc_Ecommerce1.Data;
+
+public partial class TrangWeb
+{
+    public int MaTrang { get; set; }
+
+    public string TenTrang { get; set; } = null!;
+
+    public string Url { get; set; } = null!;
+
+    public virtual ICollection<PhanQuyen> PhanQuyens { get; set; } = new List<PhanQuyen>();
+}
--- aspNetCore8Mvc_Ecommerce1/Program.cs
+++ aspNetCore8Mvc_Ecommerce1/Program.cs
@@ -1,8 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
 
+using aspNetCore8Mvc_Ecommerce1.Data;
+using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+var builder = WebApplication.CreateBuilder(args);
+//Scaffold-DbContext "Data Source=DESKTOP-L4BLRQD\SQLEXPRESS;User ID=user1;Password=123456;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Data -f
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// define db context
+builder.Services.AddDbContext<Hshop2023Context>(o =>
+{
+    o.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
